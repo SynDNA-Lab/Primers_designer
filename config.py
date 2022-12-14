16126aa087126eecacc690d9d6456820f4e947e3
Cedric K. Brinkmann <56127742+CedKB@users.noreply.github.com>
2022-12-14 13:15:44 +0100

    simplify configuration

--- config.py
+++ config.py
@@ -11,13 +11,11 @@ class Config:
     # General Settings:
     jobname: str = field(init=False)
     target_path: str = field(init=False)
-    chromosome_id: int = field(init=False)
-    output_path: str = field(init=False)
+    chromosome: str = field(init=False)
 
     # Primer Settings:
-    max_loci_offtargets: int = field(init=False, default=3)
     offtarget_size_cutoff: int = field(init=False, default=10_000) #prev. max_pcr_offtarget
-    sp_value: int = field(init=False, default=5) # number of maximal offtarget binding sites across loci
+    sponge_value: int = field(init=False, default=5) # number of maximal offtarget binding sites across loci
 
     # Paths:
     home_path: str = field(init=False, default="/home/Primer/PrimerDesigner")
@@ -30,18 +28,17 @@ class Config:
 
 
     def load(self) -> None:
-        logging.debug("Started config.py load procedure")
+        logging.info("Loading the configuraiton file ...")
         with open(self.config_path) as file:
             config = yaml.load(file, Loader=yaml.FullLoader)
 
         self.jobname = config["jobname"]
         self.target_path = config["target"]
-        self.subject_path = config["chromosome"]
-        self.output_path = config["pcr_size_target"] # fix this here
-        # check if these even exist
-        self.max_loci_offtargets = ...
-        self.offtarget_size_cutoff = ...
-        self.sp_value = ...
-        self.home_path = ...
-        self.primer3_path = ...
-        self.bowtie_path = ...
+        self.chromosome = config["chromosome"]
+        
+        self.offtarget_size_cutoff = config["offtarget_size_cutoff"] 
+        self.sponge_value = config["sponge_value"]
+
+        self.home_path = config["home"]
+        self.primer3_path = config["primer3_path"]
+        self.bowtie_path = config["bowtie_path"]
