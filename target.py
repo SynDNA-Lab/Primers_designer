e1ed2bcdefb14a82dcdb1e637dcabffce115281a
Cedric K. Brinkmann <56127742+CedKB@users.noreply.github.com>
2022-12-14 13:17:40 +0100

    backwards compatiblity

--- target.py
+++ target.py
@@ -13,20 +13,20 @@ class Target:
     record: SeqRecord = field(init=False)
     seq: str = field(init=False)
     
-    
+
     def load_record(self) -> SeqRecord:
         ''' Returns the 0-th SeqRecord'''
         file_extension = pathlib.Path(self.target_path).suffix
-        match file_extension:
-            case "gb" | "gbk":
-                rec_type = "genbank"
-            case "dna":
-                rec_type = "snapgene"
-            case "fa" | "mpfa" | "fna" | "fsa" | "fasta":
-                rec_type = "fasta"
-            case _:
-                raise ValueError(f"Unknown file extension {file_extension}")
-        
+
+        if file_extension in ["gb", "gbk"]:
+            rec_type = "genbank"
+        elif file_extension in ["dna"]:
+            rec_type = "snapgene"
+        elif file_extension in ["fa", "mpfa", "fna", "fsa", "fasta"]:
+            rec_type = "fasta"
+        else:
+            raise ValueError(f"Unknown file extension {file_extension}")
+            
         records = []
         for seq_record in SeqIO.parse(self.target_path, rec_type):
             records.append(seq_record)
